296251100e4b2734db7e15b8a9007e06ba73a92a
abhiz123 <abhirammnair@gmail.com>
2021-05-28 22:50:01 +0530

    did react routing

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+def generate_random_code():
+    length = 6
+
+    while True:
+        code = "".join(random.choices(string.ascii_uppercase,k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
+
+
+# Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8,default="",unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guess_time = models.CharField(max_length=3,default="120")
+    created_at = models.DateTimeField(auto_now_add=True)
